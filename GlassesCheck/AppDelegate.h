90b8f4ec8a67a4d9979a9d2560813ae88037a2ec
Erik Stromlund <erik@erikstromlund.com>
2015-03-27 11:12:38 +0100

    Initial Commit

---
+++ GlassesCheck/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GlassesCheck
+//
+//  Created by Erik Stromlund on 3/27/15.
+//  Copyright (c) 2015 FathomWorks LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ GlassesCheck/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  GlassesCheck
+//
+//  Created by Erik Stromlund on 3/27/15.
+//  Copyright (c) 2015 FathomWorks LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
