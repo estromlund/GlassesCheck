1893c9c6312ed36d6378e2c30fc1b1aca4551e3b
Erik Stromlund <erik@erikstromlund.com>
2015-03-30 18:29:43 +0200

    Update icons and add glasses_unknown image.

---
+++ GlassesCheck/Source/RACStream+GCHAdditions.h
@@ -0,0 +1,13 @@
+//
+//  RACStream+GCHAdditions.h
+//  GlassesCheck
+//
+//  Created by Erik Stromlund on 3/30/15.
+//  Copyright (c) 2015 FathomWorks LLC. All rights reserved.
+//
+
+#import "RACStream.h"
+
+@interface RACStream (GCHAdditions)
+
+@end
---
+++ GlassesCheck/Source/RACStream+GCHAdditions.m
@@ -0,0 +1,13 @@
+//
+//  RACStream+GCHAdditions.m
+//  GlassesCheck
+//
+//  Created by Erik Stromlund on 3/30/15.
+//  Copyright (c) 2015 FathomWorks LLC. All rights reserved.
+//
+
+#import "RACStream+GCHAdditions.h"
+
+@implementation RACStream (GCHAdditions)
+
+@end
